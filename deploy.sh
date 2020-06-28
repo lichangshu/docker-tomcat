7b28d0dc2f251b4034f45ebdf48be311eb1fba32
lichangshu <lchshu001@gmail.com>
2020-06-28 15:30:19 +0800

    add CONTEXT_PATH

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,20 @@
 CURRENT=`pwd`
 
 cd $(dirname $0)
-if [ $# != 4 ] ; then
+if [ $# -lt 4 ] ; then
   echo "ERROR ERROR ERROR : $0 app.war app_config_dir deploy_name port"
   exit 1;
 fi
 
 DOCKER_NAME=$3
 PORT=$4
+CONTEXT_PATH=ROOT
 
 set -e
+
+if [ $# -gt 4 ]; then
+  CONTEXT_PATH=$5
+fi
 
 TMP="/tmp/docker-template/"
 rm -rf $TMP
@@ -25,17 +30,16 @@ cp Dockerfile "$TMP/Dockerfile"
 cd $TMP
 
 TAG_NAME=`date +%Y%m%d-%H%M%S`
-/usr/bin/docker build . -t $DOCKER_NAME:$TAG_NAME --build-arg WAR_FILE=app.war --build-arg CONTEXT_PATH=ROOT --build-arg PORT=$PORT
+sudo /usr/bin/docker build . -t $DOCKER_NAME:$TAG_NAME --build-arg WAR_FILE=app.war --build-arg CONTEXT_PATH=$CONTEXT_PATH --build-arg PORT=$PORT
 #/usr/bin/docker build . -t $DOCKER_NAME:latest
-/usr/bin/docker tag $DOCKER_NAME:$TAG_NAME $DOCKER_NAME:latest
+sudo /usr/bin/docker tag $DOCKER_NAME:$TAG_NAME $DOCKER_NAME:latest
 
-/usr/bin/docker ps | grep $DOCKER_NAME && /usr/bin/docker stop $DOCKER_NAME
-/usr/bin/docker ps -a | grep $DOCKER_NAME && /usr/bin/docker rm $DOCKER_NAME
-/usr/bin/docker run --net host -d --restart always \
+sudo /usr/bin/docker ps | grep $DOCKER_NAME && sudo /usr/bin/docker stop $DOCKER_NAME
+sudo /usr/bin/docker ps -a | grep $DOCKER_NAME && sudo /usr/bin/docker rm $DOCKER_NAME
+sudo /usr/bin/docker run -d --net host --restart always -m 3g \
                          --name $DOCKER_NAME \
                          -e PORT=$4 \
-                         -v /var/log/$DOCKER_NAME/:/usr/local/tomcat/logs/ \
+                         -v /home/apps/tomcat-logs/$DOCKER_NAME/:/usr/local/tomcat/logs/ \
                          $DOCKER_NAME:$TAG_NAME
 
 rm -rf $TMP
-
