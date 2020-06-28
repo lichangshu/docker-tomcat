f2258eaa81c1ad211bd1736da834638bd0388e7e
lichangshu <lchshu001@gmail.com>
2020-06-28 15:35:39 +0800

    sudo /var/log/

--- deploy.sh
+++ deploy.sh
@@ -30,16 +30,16 @@ cp Dockerfile "$TMP/Dockerfile"
 cd $TMP
 
 TAG_NAME=`date +%Y%m%d-%H%M%S`
-sudo /usr/bin/docker build . -t $DOCKER_NAME:$TAG_NAME --build-arg WAR_FILE=app.war --build-arg CONTEXT_PATH=$CONTEXT_PATH --build-arg PORT=$PORT
+/usr/bin/docker build . -t $DOCKER_NAME:$TAG_NAME --build-arg WAR_FILE=app.war --build-arg CONTEXT_PATH=$CONTEXT_PATH --build-arg PORT=$PORT
 #/usr/bin/docker build . -t $DOCKER_NAME:latest
-sudo /usr/bin/docker tag $DOCKER_NAME:$TAG_NAME $DOCKER_NAME:latest
+/usr/bin/docker tag $DOCKER_NAME:$TAG_NAME $DOCKER_NAME:latest
 
-sudo /usr/bin/docker ps | grep $DOCKER_NAME && sudo /usr/bin/docker stop $DOCKER_NAME
-sudo /usr/bin/docker ps -a | grep $DOCKER_NAME && sudo /usr/bin/docker rm $DOCKER_NAME
-sudo /usr/bin/docker run -d --net host --restart always -m 3g \
+/usr/bin/docker ps | grep $DOCKER_NAME && /usr/bin/docker stop $DOCKER_NAME
+/usr/bin/docker ps -a | grep $DOCKER_NAME && /usr/bin/docker rm $DOCKER_NAME
+/usr/bin/docker run -d --net host --restart always \
                          --name $DOCKER_NAME \
                          -e PORT=$4 \
-                         -v /home/apps/tomcat-logs/$DOCKER_NAME/:/usr/local/tomcat/logs/ \
+                         -v /var/log/$DOCKER_NAME/:/usr/local/tomcat/logs/ \
                          $DOCKER_NAME:$TAG_NAME
 
 rm -rf $TMP
